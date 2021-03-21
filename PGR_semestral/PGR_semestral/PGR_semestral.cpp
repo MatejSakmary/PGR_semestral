0836055c3f61e7adacc8c8f8fc3c7d2f3e0e0199
unknown <sakmamat@fel.cvut.cz>
2021-03-22 00:44:09 +0100

    added ImGUI to the project

--- PGR_semestral/PGR_semestral/PGR_semestral.cpp
+++ PGR_semestral/PGR_semestral/PGR_semestral.cpp
@@ -1,20 +1,170 @@
-// PGR_semestral.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/**
+ * \file hello-world.cpp
+ * \brief Your first OpenGL application.
+ * \author Tomas Barak
+ */
 
+#include "imgui.h"
+#include "imgui_impl_glut.h"
+#include "imgui_impl_opengl3.h"
 #include <iostream>
+#include "pgr.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const int WIN_WIDTH = 512;
+const int WIN_HEIGHT = 512;
+const char* WIN_TITLE = "Hello World";
+
+GLuint shaderProgram = 0;
+GLuint arrayBuffer = 0;
+GLuint vao = 0;
+
+std::string vertexShaderSrc =
+"#version 140\n"
+"in vec2 position;\n"
+"void main() {\n"
+"  gl_Position = vec4(position, 0.0f, 1.0f);\n"
+"}\n"
+;
+
+std::string fragmentShaderSrc =
+"#version 140\n"
+"out vec4 color;"
+"void main() {\n"
+"  color = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+"}\n"
+;
+
+static bool show_demo_window = false;
+static bool show_another_window = true;
+static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+void timerCallback(int) {
+    glutTimerFunc(30, timerCallback, 0);
+    glutPostRedisplay();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void my_display_code()
+{
+	// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+	if (show_demo_window)
+		ImGui::ShowDemoWindow(&show_demo_window);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+	{
+		static float f = 0.0f;
+		static int counter = 0;
+
+		ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+		ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+		ImGui::Checkbox("Another Window", &show_another_window);
+
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+		ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+			counter++;
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::End();
+	}
+
+	// 3. Show another simple window.
+	if (show_another_window)
+	{
+		ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+		ImGui::Text("Hello from another window!");
+		if (ImGui::Button("Close Me"))
+			show_another_window = false;
+		ImGui::End();
+	}
+}
+void init() {
+    glClearColor(0.2f, 0.1f, 0.3f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glViewport(0, 0, glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+
+    GLuint shaders[] = {
+      pgr::createShaderFromSource(GL_VERTEX_SHADER, vertexShaderSrc),
+      pgr::createShaderFromSource(GL_FRAGMENT_SHADER, fragmentShaderSrc),
+      0
+    };
+    shaderProgram = pgr::createProgram(shaders);
+
+    static const float vertices[] = {
+      0.0f,  0.5f,
+      -0.5f, -0.5f,
+      0.5f, -0.5f,
+    };
+
+    glGenBuffers(1, &arrayBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, arrayBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    GLint positionLoc = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(positionLoc);
+    glVertexAttribPointer(positionLoc, 2, GL_FLOAT, GL_FALSE, 0, 0);
+}
+
+void draw() {
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGLUT_NewFrame();
+
+    my_display_code();
+
+    ImGuiIO& io = ImGui::GetIO();  
+    glViewport(0, 0, (GLsizei)io.DisplaySize.x, (GLsizei)io.DisplaySize.y);
+    glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    ImGui::Render();
+    glUseProgram(shaderProgram);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+
+    glutInitContextVersion(pgr::OGL_VER_MAJOR, pgr::OGL_VER_MINOR);
+    glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
+    glutCreateWindow(WIN_TITLE);
+
+    glutDisplayFunc(draw);
+     
+    glutTimerFunc(30, timerCallback, 0);
+    if (!pgr::initialize(pgr::OGL_VER_MAJOR, pgr::OGL_VER_MINOR))
+        pgr::dieWithError("pgr init failed, required OpenGL not supported?");
+
+    init();
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGLUT_Init();
+    ImGui_ImplGLUT_InstallFuncs();
+    ImGui_ImplOpenGL3_Init();
+
+    std::cout << "Hello triangle!" << std::endl;
+
+    glutMainLoop();
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui::DestroyContext();
+    return 0;
+}
